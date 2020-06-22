2bee784842dc2d9c51c5975c8aa1c591ce1aa563
Annie <annieg4123@gmail.com>
2020-06-21 18:33:51 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useRef } from "react";
+import useSearchHook from "./useSearchHook";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+  const { books, loading, hasMore, error } = useSearchHook(query, pageNumber);
+
+  const observer = useRef();
+  const lastElemRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
+
+  const handleQuery = (e) => {
+    setQuery(e.target.value);
+    setPageNumber(1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={query} onChange={handleQuery} />
+      {books.map((book, idx) => {
+        if (idx + 1 === books.length) {
+          return (
+            <div ref={lastElemRef} key={book}>
+              {book}
+            </div>
+          );
+        } else {
+          return <div key={book}>{book}</div>;
+        }
+      })}
+      {loading && <h3>Loading...</h3>}
+      {error && <h3>Error</h3>}
     </div>
   );
 }
---
+++ src/useSearchHook.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useSearchHook = (query, pageNumber) => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [hasMore, setHasMore] = useState(false);
+
+  useEffect(() => {
+    setBooks([]);
+  }, [query]);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+    let cancel;
+    axios({
+      method: "GET",
+      url: "http://openlibrary.org/search.json",
+      params: { q: query, page: pageNumber },
+      cancelToken: new axios.CancelToken((c) => (cancel = c)),
+    })
+      .then((res) => {
+        setBooks((prevBooks) => [
+          ...new Set([...prevBooks, ...res.data.docs.map((b) => b.title)]),
+        ]);
+        setHasMore(res.data.docs.length > 0);
+        setLoading(false);
+      })
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+        setError(true);
+      });
+    return () => cancel();
+  }, [query, pageNumber]);
+
+  return { books, loading, hasMore, error };
+};
+
+export default useSearchHook;
